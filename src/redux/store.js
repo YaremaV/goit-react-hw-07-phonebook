9ebeb3919a18a31a7e813c9d648cf3faaafea2cb
YaremaV <Yaremenko1996v@gmail.com>
2021-11-09 13:18:59 +0200

    delete persister

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,51 +1,37 @@
-import {
-  configureStore,
-  combineReducers,
-  getDefaultMiddleware,
-} from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
 import contactsReducer from '../redux/contacts/contacts-reducer';
 // import { composeWithDevTools } from 'redux-devtools-extension';
 // import { combineReducers } from 'redux';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+//   blacklist: ['filter'],
+// };
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   devTools: process.env.NODE_ENV === 'development',
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
+  // middleware: getDefaultMiddleware({
+  //   serializableCheck: {
+  //     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //   },
+  // }),
 });
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
 // const rootReducer = combineReducers({
 //   contacts: contactsReducer,
 // });
 
 // const store = createStore(rootReducer, composeWithDevTools());
-export default { store, persistor };
+export default { store };
 // console.log(process.env.NODE_ENV);
